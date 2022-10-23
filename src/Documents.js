0aa672902e0b8a90363c0561053f941af278f909
Jjianuo <Jwen@ucm.es>
2022-10-23 19:36:34 +0200

    esqueletos documents

---
+++ src/Documents.js
@@ -0,0 +1,25 @@
+export default class Documents extends Phaser.GameObjects.Sprite {
+	/**
+	 * Documents constructor
+	 * @param {Scene} scene - the GO's scene
+	 * @param {number} x - coordinate x
+	 * @param {number} y - coordinate y
+	 */
+	constructor(scene, x, y) {
+		super(scene, x, y, 'box');
+		this.setScale(0.5,.5);
+		this.scene.add.existing(this, true); 
+		this.body.setCollideWorldBounds();
+	}
+
+	/**
+	 * Bucle principal de la caja, comprobamos la velocidad para reducirla y setearla a 0 en ciertos umbrales
+	 * Así no se movera de manera infinita cuando la golpeemos
+	 * @param {number} t - Tiempo total
+	 * @param {number} dt - Tiempo entre frames
+	 */
+	preUpdate(t, dt) {
+		super.preUpdate(t, dt);
+
+	}
+}
