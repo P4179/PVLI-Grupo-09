5b78fe60cc043c3d4e6b601c17015f6778919380
Jjianuo <Jwen@ucm.es>
2022-10-25 11:33:36 +0200

    Certificado de autenticidad con texto

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -13,24 +13,23 @@ export default class Level1 extends Phaser.Scene {
   // Creación de los elementos que componen el nivel 1
   create() {
     // cambiar el color del fondo a blanco
-    // se instancia el reloj
-    // se pasa la fecha como un solo objeto con tres parámetros
-    new Clock(this, 100, 400, {d: 2, m: 3, y: 5});
-    // new Authenticity_Certificate(this, 500, 300, "Balls", new Date(22, 2, -235), "01001", new Date(11, 11, 2055), 'man1', 500, "aaaa");
+    this.cameras.main.backgroundColor.setTo(255, 255, 255);
+    new Clock(this, 300, 300, "23/10/2022");
+    let creaDate = new Date(22, 2, -235)
+    let expiDate = new Date(11, 11, 2055)
+    new Authenticity_Certificate(this, 600, 300, "Balls", creaDate, "01001", expiDate, 'man1', 500, "nombredeartista");
   }
 
   update(t, dt){
     // si han pasado 10 segundos se superpone la escena de la pantalla final a la actual
     // y se para la escena actual para que no se continue ejecutando
-    this.elapsed_Time += dt;
-    console.log(this.elapsed_Time);
-    if(this.elapsed_Time > 10000){
-      this.elapsed_Time = 0;
-      // se para la escena actual
-      this.scene.pause(this.scene.key);
-      // se lanza encima la pantalla final
-      this.scene.launch('end');
-    }
+    // console.log(t);
+    // if(t > 10000){
+    //   // se para la escena actual
+    //   this.scene.pause(this.scene.key);
+    //   // se lanza encima la pantalla final
+    //   this.scene.launch('end');
+    // }
   }
 
 }
