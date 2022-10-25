3546e6862079e4d4f1d2d9565a2d3fae9bfa8be3
pedroleo <pedroleo@ucm.es>
2022-10-25 07:47:53 +0200

    Clase fecha mejorada y cosas arregladas

--- src/objects/start.js
+++ src/objects/start.js
@@ -9,18 +9,18 @@ export default class Start extends Phaser.GameObjects.Text {
 	* @param {string} Texto
 	*/
 	constructor(scene, x, y) {
-		super(scene, x, y, "Press any key to start", {fontFamily: 'font1'});
+		super(scene, x, y, "Press any key to start", {fontFamily: 'ink_free'});
 		// añadir el objeto a la escena actual
 		this.scene.add.existing(this);
 		// cambiar el estilo de la fuente
 		this.setStyle({
-			fontSize: '25px',
+			fontSize: '30px',
 			color: 'black'
 		});
 		// se situa el centro de la textura en el medio
 		this.setOrigin(0.5, 0.5);
 
-		this.timer = 0;
+		this.elapsed_Time = 0;
 		// cada cuánto tiempo parpadea
 		this.flash = 1000;
 	}
@@ -29,9 +29,9 @@ export default class Start extends Phaser.GameObjects.Text {
 	// lo que se traduce en que se hace invisible y luego, visible y así todo el rato
 	// la clase Text no tiene preUpdate, no se puede llamar al del padre
 	preUpdate(t, dt) {
-		this.timer += dt;
-		if(this.timer > this.flash){
-			this.timer = 0;
+		this.elapsed_Time += dt;
+		if(this.elapsed_Time > this.flash){
+			this.elapsed_Time = 0;
 			this.visible = !this.visible;
 		}
 	}
