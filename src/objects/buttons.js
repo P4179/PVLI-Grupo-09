d018ceeaaed4f3021fc4296897e721934e5bff4c
RubiaLuque <prubia01@ucm.es>
2022-10-26 17:31:28 +0200

    mas cambios

--- src/objects/buttons.js
+++ src/objects/buttons.js
@@ -5,30 +5,27 @@ export class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y){
         super(scene, x, y);
         this.scene.add.existing(this);
-        var pointerdown=this.pointerdown;
+        
 
-        var buttonYes = this.add.image(0, 0, 'yes_button');
-        var ButtonNo = this.add.image(0, 0, 'no_button');
+        var buttonYes = this.add.sprite(this.sys.game.canvas.width/3, this.sys.game.canvas.heigth/3, 'yes_button');
+
+        var ButtonNo = this.add.sprite(this.sys.game.canvas.width/3, this.sys.game.canvas.heigth/4, 'no_button');
+
 
         buttonYes.setDisplaySize(300,147);
         buttonNo.setDisplaySize(300,147);
 
-        this.add(buttonYes);
-        this.add(buttonNo);
-
-        this.setScale(3,3);
-        
         this.scene.anims.create({
 			key: 'clickYes',
 			frames: scene.anims.generateFrameNumbers( 'yes_button', {start:0, end:1}),
-			frameRate: 5,
+			frameRate: 2,
 			repeat: 1
 		});
 
         this.scene.anims.create({
 			key: 'clickNo',
 			frames: scene.anims.generateFrameNumbers('no_button', {start:0, end:1}),
-			frameRate: 5,
+			frameRate: 2,
 			repeat: 1
 		});
 
