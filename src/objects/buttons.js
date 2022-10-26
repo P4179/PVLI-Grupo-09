6cf76591de7f92738d2a0bb8905517663365e812
RubiaLuque <prubia01@ucm.es>
2022-10-26 17:03:01 +0200

    Revert "añadido tutulo a pag web"
    This reverts commit 596ef05b077b3abc7156cd3ee586610134916a9a.

--- src/objects/buttons.js
+++ src/objects/buttons.js
@@ -1,6 +1,6 @@
 //import Boot from "../scenes/boot.js";
 
-export default class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
+export class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
     
     constructor(scene, x, y){
         super(scene, x, y);
@@ -18,14 +18,14 @@ export default class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
 
         this.setScale(3,3);
         
-        this.sprite.anims.create({
+        this.scene.anims.create({
 			key: 'clickYes',
 			frames: scene.anims.generateFrameNumbers( 'yes_button', {start:0, end:1}),
 			frameRate: 5,
 			repeat: 1
 		});
 
-        this.sprite.anims.create({
+        this.scene.anims.create({
 			key: 'clickNo',
 			frames: scene.anims.generateFrameNumbers('no_button', {start:0, end:1}),
 			frameRate: 5,
@@ -47,7 +47,7 @@ export default class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
         //Funciones que no hacen pasar la estatua
 
         });
-
+        
     }
     
     
