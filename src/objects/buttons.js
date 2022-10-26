1d43189557e30677686b22285975e7e32965216c
RubiaLuque <prubia01@ucm.es>
2022-10-26 16:53:06 +0200

    cambios botones

---
+++ src/objects/buttons.js
@@ -0,0 +1,61 @@
+//import Boot from "../scenes/boot.js";
+
+export class Buttons_Yes_No extends Phaser.GameObjects.Sprite {
+    
+    constructor(scene, x, y){
+        super(scene, x, y);
+        this.scene.add.existing(this);
+        var pointerdown=this.pointerdown;
+
+        var buttonYes = this.add.image(0, 0, 'yes_button');
+        var ButtonNo = this.add.image(0, 0, 'no_button');
+
+        buttonYes.setDisplaySize(300,147);
+        buttonNo.setDisplaySize(300,147);
+
+        this.add(buttonYes);
+        this.add(buttonNo);
+
+        this.setScale(3,3);
+        
+        this.scene.anims.create({
+			key: 'clickYes',
+			frames: scene.anims.generateFrameNumbers( 'yes_button', {start:0, end:1}),
+			frameRate: 5,
+			repeat: 1
+		});
+
+        this.scene.anims.create({
+			key: 'clickNo',
+			frames: scene.anims.generateFrameNumbers('no_button', {start:0, end:1}),
+			frameRate: 5,
+			repeat: 1
+		});
+
+        buttonYes.setInteractive();
+
+        buttonYes.on('pointerdown', pointer => {
+        
+        //Funciones que hacen pasar la estatua
+
+        });
+
+        buttonNo.setInteractive();
+
+        buttonNo.on('pointerdown', pointer => {
+        
+        //Funciones que no hacen pasar la estatua
+
+        });
+        
+    }
+    
+    
+
+
+    
+
+
+
+
+}
