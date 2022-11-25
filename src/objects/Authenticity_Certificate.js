50a8b7e56bafc29ca037c48cf1f532c7319d4b31
Jjianuo <Jwen@ucm.es>
2022-11-25 17:52:17 +0100

    Authenticity certificate rediseñado

--- src/objects/Authenticity_Certificate.js
+++ src/objects/Authenticity_Certificate.js
@@ -23,62 +23,44 @@ export default class Authenticity_Certificate extends Documents{
         this._sSerialNumber = sSerialNumber;
         this._sExpirationD = sExpirationD;
         this._sPhoto = sPhoto;
-        this._sWear = sWear;
-        this._sSculptor = sSculptor;
-		// this.initialScale = 0.4;
-		// this.setScale(this.initialScale)
 		
 		//centro: 73, 86
 		//color texto: #896789
 		//texto del nombre
-		let _sNametext = this.scene.add.text(0, -48, this._sName);
-		_sNametext.setOrigin(0.5,0.5);
+		let _sNametext = this.scene.add.text(-58, -37, this._sName);
+		_sNametext.setOrigin(0, 0);
 		_sNametext.setAlign('center');
 		_sNametext.setFill('#896789');
 		_sNametext.setFontSize(11);
 		this.add(_sNametext);
 
 		//texto del día de creacion
-		let fechaCrea = new Date(scene, -33, -25, this._sCreationD.d, this._sCreationD.m, this._sCreationD.y);
+		let fechaCrea = new Date(scene, -58, -16, this._sCreationD.d, this._sCreationD.m, this._sCreationD.y);
+		fechaCrea.setOrigin(0, 0);
 		fechaCrea.setFill('#896789');
 		fechaCrea.setFontSize(11);
 		this.add(fechaCrea);
 
 		//texto numero de serie
-		let _sSerialNumbertext = this.scene.add.text(-42, -3, this._sSerialNumber);
-		_sSerialNumbertext.setOrigin(0.5,0.5);
+		let _sSerialNumbertext = this.scene.add.text(-58, 7, this._sSerialNumber);
+		_sSerialNumbertext.setOrigin(0, 0);
 		_sSerialNumbertext.setAlign('center');
 		_sSerialNumbertext.setFill('#896789');
 		_sSerialNumbertext.setFontSize(11);
 		this.add(_sSerialNumbertext);
 
-		//texto estado de deterioro
-		let _sWeartext = this.scene.add.text(13, -3, this._sWear);
-		_sWeartext.setOrigin(0.5,0.5);
-		_sWeartext.setAlign('center');
-		_sWeartext.setFill('#896789');
-		_sWeartext.setFontSize(11);
-		this.add(_sWeartext);
-
 		//texto dia de expiracion
-		let fechaExpi = new Date(scene, -33, 18, this._sExpirationD.d, this._sExpirationD.m, this._sExpirationD.y);
+		let fechaExpi = new Date(scene, -58, 28, this._sExpirationD.d, this._sExpirationD.m, this._sExpirationD.y);
+		fechaExpi.setOrigin(0, 0);
 		fechaExpi.setFill('#896789');
 		fechaExpi.setFontSize(11);
 		this.add(fechaExpi);
 
 		//foto de la estatua
-		let photo = this.scene.add.sprite(-39, 70, this._sPhoto);
+		let photo = this.scene.add.sprite(36, 14.4, this._sPhoto);
+		photo.setOrigin(0.5, 0.5)
 		photo.setScale(0.12);
 		this.add(photo);
-
-		//firma
-		let _sSculptortext = this.scene.add.text(25, 67, this._sSculptor);
-		_sSculptortext.setOrigin(0.5,0.5);
-		_sSculptortext.setFont('Freestyle Script');
-		_sSculptortext.setAlign('center');
-		_sSculptortext.setFill('#896789');
-		_sSculptortext.setFontSize(11);
-		this.add(_sSculptortext);
 
     }
 
