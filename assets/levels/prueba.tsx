5364155271411f257fae692ebb1258b650c88f0d
maria <marsache@ucm.es>
2022-12-14 23:48:58 +0100

    nivel cajas 1

---
+++ assets/levels/prueba.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="prueba" tilewidth="24" tileheight="33" tilecount="135" columns="5">
+ <image source="prueba.png" width="128" height="896"/>
+</tileset>
