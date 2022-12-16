df267f7ee098398433be79acbc13d77ffed79fc0
Jjianuo <Jwen@ucm.es>
2022-12-16 04:34:24 +0100

    Fisicas? puzzle 2

---
+++ assets/levels/prueba.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="prueba" tilewidth="24" tileheight="33" tilecount="135" columns="5">
+ <image source="prueba.png" width="128" height="896"/>
+ <tile id="20">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
