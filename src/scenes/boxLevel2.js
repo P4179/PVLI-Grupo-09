77ce121acbbd8e0cbdd2b0073f27de832211225a
Jjianuo <Jwen@ucm.es>
2022-12-16 04:34:24 +0100

    Fisicas? puzzle 2

--- src/scenes/boxLevel2.js
+++ src/scenes/boxLevel2.js
@@ -21,29 +21,52 @@ export default class BoxLevel2 extends Phaser.Scene {
 
         this.background = this.map.createLayer('background', tileset);
         this.staticlayer = this.map.createLayer('walls', tileset);
+        // this.staticlayer.setCollision(5);
 
         this.staticlayer.setCollisionByProperty({ colisiona: true });
         
         this.boxes = this.map.createFromObjects('boxes', {name: 'box', key: 'm_box'});
         this.boxes.map(b => b.y += 33);
         this.boxes.map(b => this.physics.add.existing(b));
-        this.boxes.map(b => b.body.setImmovable(true));
+        this.boxes.map(b => b.body.setSize(b.width * 0.9, b.height * 0.9));
 
         this.cameras.main.scrollX = -(CANVAS_WIDTH / 2 - this.map.widthInPixels / 2);
         this.cameras.main.scrollY = -(CANVAS_HEIGHT / 2 - this.map.heightInPixels  / 2);
+        this.cameras.main.setZoom(1.60);
 
-        // this.boxlayer.setCollisionByProperty({colli: true});
-        // this.physics.add.collider(this.player, this.boxlayer);
+        this.character = new Character(this, this.map.widthInPixels / 2, this.map.heightInPixels - 10);
 
-        this.character = new Character(this, 100, 100);
+        this.physics.add.collider(this.boxes, this.boxes, (box1, box2) => {
+            console.log(box1);
+            console.log(box2);
+            box1.body.setVelocity(0);
+            box2.body.setVelocity(0);
+        });
+        this.physics.add.collider(this.boxes, this.staticlayer);
+        this.physics.add.collider(this.character, this.staticlayer);
+        this.physics.add.collider(this.character, this.boxes, (character, box) => {
+            if(box.body.touching.left) {
+                box.body.setVelocityX(+20)
+            }
+            else if(box.body.touching.right) {
+                box.body.setVelocityX(-20)
+            }
+            if(box.body.touching.up) {
+                box.body.setVelocityY(+20)
+            }
+            if(box.body.touching.down) {
+                box.body.setVelocityY(-20)
+            }
 
-        this.box = new Box(this, 200, 200, 100);
+        });
+        
+        // this.box = new Box(this, 200, 200, 100);
 
         new Reset(this, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
 
-        this.box.on('hasReached', () => {
-            this.character.enabledInput(true);
-        });
+        // this.box.on('hasReached', () => {
+        //     this.character.enabledInput(true);
+        // });
 
         this.physics.add.collider(this.character, this.box, (character, box) => {
             this.time.addEvent( {
